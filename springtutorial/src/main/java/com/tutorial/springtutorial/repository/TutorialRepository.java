2bf874c79f914f1ec41d564a08eaead52062de09
Perebati <lucas.bpereira1999@gmail.com>
2023-07-14 20:31:12 -0300

    intelij commit

--- springtutorial/src/main/java/com/tutorial/springtutorial/repository/TutorialRepository.java
+++ springtutorial/src/main/java/com/tutorial/springtutorial/repository/TutorialRepository.java
@@ -1,2 +1,13 @@
-package com.tutorial.springtutorial.repository;public class TutorialRepository {
+package com.tutorial.springtutorial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tutorial.springtutorial.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
 }
